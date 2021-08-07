e36dbd77109d43abb9aad75700d6886f783c25e6
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-07 06:58:28 +0530

    Sending a dispatch

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -9,19 +9,44 @@ import {
 } from "reactstrap";
 
 import { v4 } from "uuid";
-import {} from "../Context/TodoContext";
+import { TodoContext } from "../Context/TodoContext";
 import { ADD_TODO } from "../Context/action.types";
 
 const TodoForm = () => {
+    const [todoString, setTodoString] = useState("");
+    const {dispatch} = useContext(TodoContext);
+
+const handleSubmit = e => {
+    e.preventDefault();
+    if (todoString === "") {
+        return alert("Please enter a todo");
+    }
+
+    const todo = {
+        todoString,
+        id: v4()
+    }
+
+    dispatch({
+        type: ADD_TODO,
+        payload: todo
+    })
+
+    setTodoString("")
+}
+
+
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
       <InputGroup> <Input
       type="text"
       name="todo"
       id="todo"
       placeholder="Your next todo"
-      // Todo: value, onChange
+      value={todoString}
+      onChange={e => setTodoString(e.target.value)}
     />
     <InputGroupAddon addonType="prepend">
     <Button color="warning" 
