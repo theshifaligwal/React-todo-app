fbbb29435ba66c5cf70e468d5ed0bff8b8dfd717
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-07 06:16:08 +0530

    UseReducer for App

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -1,15 +1,13 @@
-import {ADD_TODO, REMOVE_TODO} from "./action.types"
+import { ADD_TODO, REMOVE_TODO } from "./action.types";
 
 export default (state, action) => {
-    switch (action.type) {
-        case ADD_TODO:
-            return [...state, action.payload]
-            case REMOVE_TODO:
-                return state.filter(todo => todo.id !== action.payload)
-            
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.payload];
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.payload);
 
-    
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
